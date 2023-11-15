3c57c239dd4de95e396c0606016c9101eff06f65
guka19 <22100570@ibsu.edu.ge>
2023-11-15 12:52:58 +0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SigninComponent } from './pages/auth/signin/signin.component';
+import { SignupComponent } from './pages/auth/signup/signup.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: IndexComponent },
+  { path: "", component: MenuComponent },
+  { path: "about", component: AboutComponent },
+  { path: "signin", component: SigninComponent },
+  { path: "signup", component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IndexComponent } from './pages/index/index.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SignupComponent } from './pages/auth/signup/signup.component';
+import { SigninComponent } from './pages/auth/signin/signin.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IndexComponent,
+    AboutComponent,
+    SignupComponent,
+    SigninComponent,
+    FooterComponent,
+    ButtonComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
